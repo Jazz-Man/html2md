62beae6f4a2cf980bbbf4ca81eff7e906ffde63d
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-05-11 22:16:03 +0300

    Add HTMLRewriter pipeline and project structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log("Hello via Bun TEST!");
+import { defaultIgnoreElements } from "./config";
+import { getSorsCode, inputHtml, outputMd, saveFile } from "./utils";
+
+// Replace all images with a rickroll
+const rewriter = new HTMLRewriter()
+	.on(defaultIgnoreElements.join(","), {
+		element(element) {
+			element.remove();
+		},
+	})
+	.on("img", {
+		element(img) {
+			// Famous rickroll video thumbnail
+			img.setAttribute(
+				"src",
+				"https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
+			);
+
+			// Wrap the image in a link to the video
+			img.before(
+				'<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">',
+				{
+					html: true,
+				},
+			);
+			img.after("</a>", { html: true });
+
+			// Add some fun alt text
+			img.setAttribute("alt", "Definitely not a rickroll");
+		},
+	});
+
+rewriter.onDocument({
+	doctype(doctype) {
+		doctype.remove();
+	},
+	comments(comment) {
+		comment.remove();
+	},
+});
+
+// An example HTML document
+const html = await getSorsCode(inputHtml);
+
+const result = rewriter.transform(html);
+
+await saveFile(outputMd, result);
---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+import { resolve } from "node:path";
+
+const rootDir = process.cwd();
+
+export const inputHtml = resolve(rootDir, "test.html");
+
+export const outputMd = resolve(rootDir, "test.md");
+
+export const getSorsCode = async (input: string) =>
+	await Bun.file(input).text();
+
+export const saveFile = async (path: Bun.PathLike, content: string) => {
+	await Bun.write(path, content, {
+		createPath: true,
+	});
+};
